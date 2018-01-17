d42643cb2161e86de1545df5e763f3da8ed8dfb7
Shagun Madhikarmi <madhikarma@gmail.com>
2018-01-17 07:09:40 +0000

    IndecisionApp: form submission

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,7 +5,15 @@ console.log('App.js is running!');
 var app = {
     title: 'Indecision App',
     subtitle: 'A react web app for making decisions',
-    options: ['One', 'Two']
+    options: []
+};
+var onFormSubmit = function onFormSubmit(event) {
+    event.preventDefault();
+    var option = event.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+    }
+    event.target.elements.option.value = ' ';
 };
 var template = React.createElement(
     'div',
@@ -26,65 +34,21 @@ var template = React.createElement(
         app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
-        'ol',
+        'p',
         null,
+        app.options.length
+    ),
+    React.createElement(
+        'form',
+        { onSubmit: onFormSubmit },
+        React.createElement('input', { type: 'text', name: 'option' }),
         React.createElement(
-            'li',
+            'button',
             null,
-            'item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'item two'
+            'Add option'
         )
     )
 );
 
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
-};
-
 var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Count: ',
-            count
-        ),
-        React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: reset },
-            'Reset'
-        )
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,47 +3,33 @@ console.log('App.js is running!');
 const app = {
     title: 'Indecision App',
     subtitle: 'A react web app for making decisions',
-    options: ['One', 'Two']
+    options: []
 }
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    const option = event.target.elements.option.value
+    if (option) {
+        app.options.push(option);
+    }
+    event.target.elements.option.value = ' ';
+};
 const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
+        <p>{app.options.length}</p>
+
+        {/* <ol>
             <li>item one</li>
             <li>item two</li>
-        </ol>
+        </ol> */}
+        <form onSubmit={onFormSubmit}>
+            <input type="text" name="option"/>
+            <button>Add option</button>
+        </form>
     </div>
 );
 
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-const minusOne = () => {
-    count --;
-    renderCounterApp();
-};
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
-
 const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);

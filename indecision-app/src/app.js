4cc5e4aa8be7f0dde2ae7248e46b7b61dbb434a4
Shagun Madhikarmi <madhikarma@gmail.com>
2018-01-17 06:49:59 +0000

    IndecisionApp: count example: re-rendering React on interaction

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -19,26 +19,31 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-    count += 1;
-    // console.log('addOne');
+    count++;
+    renderCounterApp();
 };
 const minusOne = () => {
-    count -= 1;
-    // console.log('minusOne');
+    count --;
+    renderCounterApp();
 };
 const reset = () => {
     count = 0;
-    // console.log('reset');
+    renderCounterApp();
 };
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>Reset</button>
-    </div>
-);
-console.log(templateTwo);
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();

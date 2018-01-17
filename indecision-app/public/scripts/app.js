c7395b292221849c4c22e5265cf3bf696bb09fac
Shagun Madhikarmi <madhikarma@gmail.com>
2018-01-17 07:12:11 +0000

    IndecisionApp: refactor tto use a render function that we can reuse on data changes (due to no data binding in JSX without React components

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -14,41 +14,64 @@ var onFormSubmit = function onFormSubmit(event) {
         app.options.push(option);
     }
     event.target.elements.option.value = ' ';
+    renderApp();
 };
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length
-    ),
-    React.createElement(
-        'form',
-        { onSubmit: onFormSubmit },
-        React.createElement('input', { type: 'text', name: 'option' }),
-        React.createElement(
-            'button',
-            null,
-            'Add option'
-        )
-    )
-);
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+renderApp();
+
+// create render func that renders new jsx
+// call on init
+// call it after options array is added to
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -12,24 +12,32 @@ const onFormSubmit = (event) => {
         app.options.push(option);
     }
     event.target.elements.option.value = ' ';
+    renderApp();
 };
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <p>{app.options.length}</p>
-
-        {/* <ol>
-            <li>item one</li>
-            <li>item two</li>
-        </ol> */}
-        <form onSubmit={onFormSubmit}>
-            <input type="text" name="option"/>
-            <button>Add option</button>
-        </form>
-    </div>
-);
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>item one</li>
+                <li>item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+renderApp();
+
+// create render func that renders new jsx
+// call on init
+// call it after options array is added to

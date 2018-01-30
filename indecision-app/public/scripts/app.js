e037e76ab3d1cc892ee4c313c4622a144a6dafc6
Shagun Madhikarmi <shagun@ustwo.com>
2018-01-30 08:29:53 +0000

    IndecisionApp: Updated Person class to accept age (Section 4 React Components - 24 ES6 Classes Part 1)

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,86 +1,42 @@
 'use strict';
 
-console.log('App.js is running!');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {
-    title: 'Indecision App',
-    subtitle: 'A react web app for making decisions',
-    options: []
-};
-var onFormSubmit = function onFormSubmit(event) {
-    event.preventDefault();
-    var option = event.target.elements.option.value;
-    if (option) {
-        app.options.push(option);
-        event.target.elements.option.value = ' ';
-        renderApp();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// Setup constructor to habe name and age (default to 0)
+// getDescription = "Andrew Mead is ${age} year(s) old.""
+
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
     }
-};
 
-var onRemoveAll = function onRemoveAll() {
-    app.options = [];
-    renderApp();
-};
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old.';
+        }
+    }]);
 
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * app.options.length);
-    var option = app.options[randomNum];
-    alert(option);
-};
+    return Person;
+}();
 
-var appRoot = document.getElementById('app');
+var me = new Person('Andrew Mead', 26);
+console.log(me.getGreeting());
+console.log(me.getDescription());
 
-var renderApp = function renderApp() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            app.title
-        ),
-        app.subtitle && React.createElement(
-            'p',
-            null,
-            app.subtitle
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
-        ),
-        React.createElement(
-            'button',
-            { disabled: app.options.length == 0, onClick: onMakeDecision },
-            'What should I do?'
-        ),
-        React.createElement(
-            'button',
-            { onClick: onRemoveAll },
-            'Remove All'
-        ),
-        React.createElement(
-            'ol',
-            null,
-            app.options.map(function (option) {
-                return React.createElement(
-                    'li',
-                    { key: option },
-                    option
-                );
-            })
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                'Add option'
-            )
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
-renderApp();
+var other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -2,15 +2,21 @@
 // getDescription = "Andrew Mead is ${age} year(s) old.""
 
 class Person {
-    constructor(name = 'Anonymous') {
+    constructor(name = 'Anonymous', age = 0) {
         this.name = name;
+        this.age = age;
     }
     getGreeting() {
         return `Hi. I am ${this.name}!`;
     }
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`;
+    }
 }
-const me = new Person('Andrew Mead');
+const me = new Person('Andrew Mead', 26);
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
 console.log(other.getGreeting());
+console.log(other.getDescription());

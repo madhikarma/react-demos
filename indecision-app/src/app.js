9482f83a76ec7b3587b5d0580e633a9b67bfff9f
Shagun Madhikarmi <madhikarma@gmail.com>
2018-02-04 22:01:29 +0000

    IndecisionApp: Cleanup using implicit returns  (Section 5 Stateless Functional Components - 43 Removing Individual Options)

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -9,11 +9,7 @@ class IndecisionApp extends React.Component {
         }
     }
     handleDeleteOptions() {
-        this.setState( () => {
-            return ({ 
-                options: [] 
-            });
-        })
+        this.setState(() => ({ options: [] }));
     }
     handlePick() {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
@@ -26,11 +22,7 @@ class IndecisionApp extends React.Component {
         } else if (this.state.options.indexOf(option) > -1) {
             return 'This option already exists'
         } 
-        this.setState( (prevState) => {
-            return ( {
-                options: prevState.options.concat([option])
-            })
-        })
+        this.setState((prevState) => ({ options: prevState.options.concat([option]) }));
     }
     render() {
         const subtitle = 'Put your life in the hands of a computer';
@@ -101,13 +93,9 @@ class AddOption extends React.Component {
     }
     handleAddOption(event) {
         event.preventDefault();
-        
         const option = event.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
-
-        this.setState( () => {
-            return { error };
-        });
+        this.setState( () => ({ error }) );
     };    
     render() {
         return (

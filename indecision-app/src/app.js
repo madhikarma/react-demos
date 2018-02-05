a323e64f27a4ddd102e9ce7313ef5b74cb4a8b7d
Shagun Madhikarmi <madhikarma@gmail.com>
2018-02-05 02:10:10 +0000

    IndecisionApp: Setting up Webpack with SCSS (Section 8 Styling React 64 Setting up Webpack)

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-
-// const Layout = (props) => {
-//   return (
-//     <div>
-//       <p>Header</p>
-//       {props.content}
-//       <p>Footer</p>
-//     </div>
-//   );
-// };
-// ReactDOM.render(
-//   <Layout>
-//     <div>
-//     <h1>Page title</h1>
-//     <p>This is my page</p>
-//   </div>
-//   </Layout>, 
-//   document.getElementById('app')
-// );
+import './styles/styles.css'; 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -8,10 +8,18 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
+    rules: [
+    {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
+    }, 
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map',

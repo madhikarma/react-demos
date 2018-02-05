93a185bd523c8731477da3363fa268d052cc81b7
Shagun Madhikarmi <madhikarma@gmail.com>
2018-02-05 01:24:30 +0000

    IndecisionApp: Child props (Section 7 Installing 3rd party dep- 60 Child props)

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
+// const Layout = (props) => {
+//   return (
+//     <div>
+//       <p>Header</p>
+//       {props.content}
+//       <p>Footer</p>
+//     </div>
+//   );
+// };
+// ReactDOM.render(
+//   <Layout>
+//     <div>
+//     <h1>Page title</h1>
+//     <p>This is my page</p>
+//   </div>
+//   </Layout>, 
+//   document.getElementById('app')
+// );
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-  constructor() {
-    this.name = "Mike";
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `Hi my name is ${this.name}`;
-  }
-}
-
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting;
-console.log(getGreeting());
-
-class NewSyntax {
-  name = 'Jen';
-  getGreeting = () => {
-    return `Hi my name is ${this.name}`; 
-  }
-}
-
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());

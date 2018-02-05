bd0cc785887e266d67d55cb558280343231aee89
Shagun Madhikarmi <madhikarma@gmail.com>
2018-02-05 02:16:20 +0000

    IndecisionApp: Converted CSS to SCSS and using Webpack to help load and also for browsers to understand (Section 8 Styling React 64 Setting up Webpack)

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -15,10 +15,11 @@ module.exports = {
       exclude: /node_modules/
     }, 
     {
-      test: /\.css$/,
+      test: /\.scss$/,
       use: [
         'style-loader',
-        'css-loader'
+        'css-loader',
+        'sass-loader'
       ]
     }]
   },
